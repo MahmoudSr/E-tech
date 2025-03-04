2dfea94da119ebe5e93f0db7fc27f275e73ea3de
mahmoud srouji <srouji.mahmoud@gmail.com>
2025-03-04 23:57:45 +0200

    added main styling and ppages with one style.css file need to organize the style files

---
+++ script.js
@@ -0,0 +1,75 @@
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+// Check saved mode preference when the page loads
+if (localStorage.getItem("mode") === "dark") {
+  document.body.classList.add("dark-mode");
+  darkModeToggle.textContent = "☀️";
+} else {
+  document.body.classList.remove("dark-mode");
+  darkModeToggle.textContent = "🌙";
+}
+
+// Dark Mode Toggle Event Listener
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  // Save the mode preference in localStorage
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("mode", "dark");
+    darkModeToggle.textContent = "☀️";
+    darkModeToggle.style.color = "#fff";
+  } else {
+    localStorage.setItem("mode", "light");
+    darkModeToggle.textContent = "🌙";
+    darkModeToggle.style.color = "#000";
+  }
+});
+
+const hamburger = document.getElementById("hamburger");
+const overlay = document.getElementById("overlay");
+const navMenu = document.querySelector("#nav-menu");
+const menuLinks = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  overlay.style.display = navMenu.classList.contains("active")
+    ? "block"
+    : "none";
+});
+
+overlay.addEventListener("click", () => {
+  navMenu.classList.remove("active");
+  overlay.style.display = "none";
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+    overlay.style.display = "none";
+  });
+});
+
+// Add this script to handle the collapsible FAQ behavior
+document.querySelectorAll(".faq-toggle").forEach((button) => {
+  button.addEventListener("click", () => {
+    const faqItem = button.closest(".faq-item");
+    faqItem.classList.toggle("open");
+  });
+});
+
+// emailjs.init("YOUR_PUBLIC_KEY"); // Get from EmailJS Dashboard
+
+// document
+//   .getElementById("contact-form")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault();
+
+//     emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this).then(
+//       function (response) {
+//         alert("Email sent successfully!");
+//       },
+//       function (error) {
+//         alert("Failed to send email. Try again later.");
+//       }
+//     );
+//   });
