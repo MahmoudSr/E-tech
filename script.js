a683877c3cab63ca27074e4af9f6a4991567769f
mahmoud srouji <srouji.mahmoud@gmail.com>
2025-03-05 00:12:38 +0200

    added view-port for notch, need to add safe ares later!!

--- script.js
+++ script.js
@@ -56,20 +56,3 @@ document.querySelectorAll(".faq-toggle").forEach((button) => {
     faqItem.classList.toggle("open");
   });
 });
-
-// emailjs.init("YOUR_PUBLIC_KEY"); // Get from EmailJS Dashboard
-
-// document
-//   .getElementById("contact-form")
-//   .addEventListener("submit", function (event) {
-//     event.preventDefault();
-
-//     emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this).then(
-//       function (response) {
-//         alert("Email sent successfully!");
-//       },
-//       function (error) {
-//         alert("Failed to send email. Try again later.");
-//       }
-//     );
-//   });
